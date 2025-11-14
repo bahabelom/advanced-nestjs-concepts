cc7565a27fead5d562def20f64e1344fb89f2df5
bahabelom <bahabelomgebremedhn2@gmail.com>
2025-11-14 15:09:07 +0300

    added cach manager and users module to practice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+
+    CacheModule.register({
+      ttl: 5000,
+      max: 10,
+    }),
+
+    UserModule,
+
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
